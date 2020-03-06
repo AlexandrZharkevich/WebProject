f10ee702f4d24aedc014d3d26589ae4d15a0a83c
Aleksandr Zharkevich <alexandr.zharkevich@gmail.com>
2020-03-06 21:56:34 +0300

    First Servlet

--- src/by/mrbregovich/LoginServlet.java
+++ src/by/mrbregovich/LoginServlet.java
@@ -1,13 +1,27 @@
 package by.mrbregovich;
 
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/login")
 public class LoginServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Good morning </title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("First Servlet");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 }
